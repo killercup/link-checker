4d951cbb559c7e5df708146672ca30d1b66337a0
Pascal Hertleif <killercup@gmail.com>
2015-11-26 15:41:24 +0100

    A Happy Regex Union

--- src/lint.rs
+++ src/lint.rs
@@ -2,16 +2,19 @@ use std::collections::HashSet;
 use regex::Regex;
 
 pub fn lint_html_links(html: &str) -> Result<(), HashSet<String>> {
-    let link_regex = Regex::new("href=(\"|')#(?P<id>.*?)(\"|')").unwrap();
-    let id_regex = Regex::new("id=(\"|')(?P<id>.*?)(\"|')").unwrap();
+    // let link_regex = Regex::new("href=(\"|')#(?P<id>.*?)(\"|')").unwrap();
+    // let id_regex = Regex::new("id=(\"|')(?P<id>.*?)(\"|')").unwrap();
 
-    let links: HashSet<&str> = link_regex.captures_iter(html)
-                                         .filter_map(|cap| cap.name("id"))
-                                         .collect();
+    let regex = Regex::new("href=(\"|')#(?P<anchor_link>.*?)(\"|')|id=(\"|')(?P<id>.*?)(\"|')")
+                    .unwrap();
 
-    let ids: HashSet<&str> = id_regex.captures_iter(html)
-                                     .filter_map(|cap| cap.name("id"))
-                                     .collect();
+    let links: HashSet<&str> = regex.captures_iter(html)
+                                    .filter_map(|cap| cap.name("anchor_link"))
+                                    .collect();
+
+    let ids: HashSet<&str> = regex.captures_iter(html)
+                                  .filter_map(|cap| cap.name("id"))
+                                  .collect();
 
     if links.is_subset(&ids) {
         Ok(())
